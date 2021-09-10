64aa6f3d6471f0c58ca92733384cba8de22dbc61
yaaltechnologies <yaaltechnologies@gmail.com>
2021-09-10 12:53:20 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Vaanibam_mobile',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
